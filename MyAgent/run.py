a265d520215365a1a6967cb8a0e3b4d74ab5fb89
Brain <562966616@qq.com>
2018-11-21 00:31:27 +0800

    fix run.py used in Dockerfile

--- MyAgent/run.py
+++ MyAgent/run.py
@@ -1,15 +1,18 @@
 """Implementation of a simple deterministic agent using Docker."""
-from NetAgent import *
+from conv_agent import convNetwork
 from pommerman.runner import DockerAgentRunner
 import tensorflow as tf
 import joblib
+from pommerman.agents import SimpleAgent, BaseAgent
+import pommerman
 
 
 class MyAgent(DockerAgentRunner):
     '''An example Docker agent class'''
 
     def __init__(self,sess, params):
-        self._agent = NetAgent(sess, params)
+        self._agent = convNetwork(sess, params)
+
 
     def init_agent(self, id, game_type):
         return self._agent.init_agent(id, game_type)
@@ -27,11 +30,11 @@ class MyAgent(DockerAgentRunner):
 def main():
     '''Inits and runs a Docker Agent'''
     sess = tf.InteractiveSession()
-    path = 'model_3_policy'
+    path = 'parametersepoch8'
     params = joblib.load(path)
     agent = MyAgent(sess, params)
     agent.run()
 
-
 if __name__ == "__main__":
     main()
+
